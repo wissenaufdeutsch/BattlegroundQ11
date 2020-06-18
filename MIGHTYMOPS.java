58f9808979421c839bb4495f99916d9d4caeaff6
Marvin <marvino2012@gmx.de>
2020-06-18 12:36:55 +0200

    MIGHTYMOPS joins the arena.

---
+++ MIGHTYMOPS.java
@@ -0,0 +1,31 @@
+
+public class MIGHTYMOPS extends CONTENDER
+{
+
+    /**
+     * Constructor for objects of class CHICKEN
+     */
+    public MIGHTYMOPS()
+    {
+        super();
+        SetzeName("MIGHTYMOPS");
+        SetzeBeschreibung("");
+    }
+    
+    public double gibWkeitPapier()
+    {
+        return 0.2;
+    }
+    
+    public double gibWkeitSchere()
+    {
+        return 0.2;
+    }
+    
+    public double gibWkeitStein()
+    {
+        return 0.6;
+    }
+    
+
+}
