55850839be284c65defbbe522d01308efcd77817
Jannis <jannis-brauer@web.de>
2020-06-18 12:27:18 +0200

    Schlechte Spielerin hinzugefügt

---
+++ KARIN.java
@@ -0,0 +1,25 @@
+
+
+public class KARIN extends CONTENDER
+{
+    
+    public KARIN()
+    {
+       super();
+    }
+
+    public double gibWkeitSchere()
+    {
+        return 0.9;
+    }
+    
+    public double gibWkeitStein()
+    {
+        return 0.1;
+    }
+    
+    public double gibWkeitPapier()
+    {
+        return 0.0;
+    }
+}
