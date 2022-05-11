ed02dae3d154ecde4aaec511a1a2f310d10d2bf5
Michael Koopmans <wissenaufdeutsch@gmail.com>
2022-05-11 08:36:03 +0200

    Deleted old stuff

--- START.java
+++ START.java
@@ -23,9 +23,7 @@ public class START
     {
         //Hier müssen neue Teilnehmer 
         CONTENDER c = new CHICKEN();
-
-       
-        b.TeilnehmerHinzu(c); //Fuegt CHICKEN/PLATYPUS zu den Teilnehmern hinzu
+        b.TeilnehmerHinzu(c); //Fuegt CHICKEN zu den Teilnehmern hinzu
 
       
 
