293e7481a7155464c623a8c0a723b08a1cd3b989
Kili <Kili.Schreiner@gmail.com>
2022-05-11 08:54:44 +0200

    Start

--- START.java
+++ START.java
@@ -25,21 +25,28 @@ public class START
         //Hier müssen neue Teilnehmer 
         CONTENDER c = new CHICKEN();
         b.TeilnehmerHinzu(c);
+        
         CONTENDER w = new WASCHBÄR();
         b.TeilnehmerHinzu(w);
-
-
-        CONTENDER ws= new WILLSMITH();
         
-      b.TeilnehmerHinzu(ws); //Fuegt WILLSMITH hinzu
-
-
-       
+        CONTENDER ws= new WILLSMITH();
+        b.TeilnehmerHinzu(ws);
+        //Fuegt WILLSMITH hinzu
         CONTENDER k = new SCHUELER("Kilian");
         b.TeilnehmerHinzu(k);
-
-
-
+        
+        
+        CONTENDER ch = new CHEATA();
+        b.TeilnehmerHinzu(ch);
+        
+         CONTENDER m = new MIAMI();
+        b.TeilnehmerHinzu(m);
+        
+        CONTENDER p = new PLATYPUS();
+        b.TeilnehmerHinzu(p);
+        
+        CONTENDER ko = new KOOBI_WAN_KENOBI();
+        b.TeilnehmerHinzu(ko);
     }
     /**
      * DONT CHANGE THIS!
