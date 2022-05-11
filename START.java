4107b08b6245840272dfcced27fab09612cb7c65
Kili <Kili.Schreiner@gmail.com>
2022-05-11 08:26:05 +0200

    Start angepasst

--- START.java
+++ START.java
@@ -22,25 +22,10 @@ public class START
     public void InitContenders()
     {
         //Hier müssen neue Teilnehmer 
-        CONTENDER c = new CHICKEN();
-
-        CONTENDER p = new PLATYPUS();
-        b.TeilnehmerHinzu(c); //Fuegt CHICKEN/PLATYPUS zu den Teilnehmern hinzu
-        b.TeilnehmerHinzu(p);
-
-        CONTENDER d = new BRAINY();
-        b.TeilnehmerHinzu(d);
-
-        CONTENDER m = new MARCELLO();
-        b.TeilnehmerHinzu(m);
         
-        CONTENDER f = new MASTERMIND();
-        b.TeilnehmerHinzu(f);
-         CONTENDER l = new Logan();
-        b.TeilnehmerHinzu(l);
-        CONTENDER v = new Captian_Vor();
+        CONTENDER v = new CHICKEN();
         b.TeilnehmerHinzu(v);
-
+    
     }
 
     /**
