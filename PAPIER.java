79221543d9d6f138238fe663f5c09fdae2fcaa35
Michael Koopmans <wissenaufdeutsch@gmail.com>
2022-05-11 08:56:33 +0200

    PAPIER IS THERE!

---
+++ PAPIER.java
@@ -0,0 +1,32 @@
+
+
+
+public class PAPIER extends CONTENDER
+{
+    /**
+     * Constructor for objects of class CHICKEN
+     */
+    public PAPIER()
+    {
+        super();
+        SetzeName("Papier");
+        SetzeBeschreibung("Ein Stück Papier, was hast du erwartet?");
+    }
+    
+    public double gibWkeitPapier()
+    {
+        return 1;
+    }
+    
+    public double gibWkeitSchere()
+    {
+        return 0;
+    }
+    
+    public double gibWkeitStein()
+    {
+        return 0;
+    }
+    
+
+}
