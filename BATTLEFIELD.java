583b7e3f3fbdc4dfb0ffb05868fdddc8b3aefd68
Felo <fungerhofer@gmail.com>
2022-05-11 09:15:16 +0200

    Platzierugn fix fehlgeschlagen -> wieder zurück

--- BATTLEFIELD.java
+++ BATTLEFIELD.java
@@ -51,7 +51,7 @@ public class BATTLEFIELD implements Runnable
         pauseFuerAnimation(2000);
         for(int k = 0; k<model.GibAnzahlTeilnehmer(); k=k+1)
         {
-            d.Endergebnis(k+1, model.GibName(k), model.GibPunkte(k));
+            d.Endergebnis(0, model.GibName(k), model.GibPunkte(k));
             pauseFuerAnimation(1000);
         }
         
