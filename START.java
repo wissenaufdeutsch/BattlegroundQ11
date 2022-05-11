bc1a78eeac52cfc09f39cec37b3c8c01ea330883
Michael Koopmans <wissenaufdeutsch@gmail.com>
2022-05-11 08:20:07 +0200

    Deleted contenders removed from START class

--- START.java
+++ START.java
@@ -24,22 +24,10 @@ public class START
         //Hier müssen neue Teilnehmer 
         CONTENDER c = new CHICKEN();
 
-        CONTENDER p = new PLATYPUS();
+       
         b.TeilnehmerHinzu(c); //Fuegt CHICKEN/PLATYPUS zu den Teilnehmern hinzu
-        b.TeilnehmerHinzu(p);
 
-        CONTENDER d = new BRAINY();
-        b.TeilnehmerHinzu(d);
-
-        CONTENDER m = new MARCELLO();
-        b.TeilnehmerHinzu(m);
-        
-        CONTENDER f = new MASTERMIND();
-        b.TeilnehmerHinzu(f);
-         CONTENDER l = new Logan();
-        b.TeilnehmerHinzu(l);
-        CONTENDER v = new Captian_Vor();
-        b.TeilnehmerHinzu(v);
+      
 
     }
 
