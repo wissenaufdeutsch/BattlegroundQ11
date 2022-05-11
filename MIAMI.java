7a8361f655f53a4ae6d795ca2ce26eb42fcbba2c
Stefanie <stefanie.auerswald@web.de>
2022-05-11 08:46:10 +0200

    MIAMI JOINED

---
+++ MIAMI.java
@@ -0,0 +1,28 @@
+
+
+public class MIAMI extends CONTENDER
+{
+    public MIAMI()
+    {
+        super();
+        SetzeName("MIAMI");
+        SetzeBeschreibung("Wenn ihr nicht nach Miami kommt, muss Miami eben zu euch kommen!");
+    }
+    
+    public double gibWkeitPapier()
+    {
+        return 0.2;
+    }
+    
+    public double gibWkeitSchere()
+    {
+        return 0.45;
+    }
+    
+    public double gibWkeitStein()
+    {
+        return 0.35;
+    }
+    
+
+}
