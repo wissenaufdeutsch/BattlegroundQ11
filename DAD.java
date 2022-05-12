162639c6e67f9714a4e9ecd3d2ba9980b67b7afe
Michael Koopmans <wissenaufdeutsch@gmail.com>
2022-05-12 14:12:42 +0200

    DAD plays too

---
+++ DAD.java
@@ -0,0 +1,28 @@
+
+
+
+public class DAD extends CONTENDER
+{
+    public DAD()
+    {
+        super();
+        SetzeName("DAD");
+        SetzeBeschreibung("Mir fällt kein DAD Joke ein...");
+    }
+    
+    public double gibWkeitPapier()
+    {
+        return 0.4;
+    }
+    
+    public double gibWkeitSchere()
+    {
+        return 0.4;
+    }
+    
+    public double gibWkeitStein()
+    {
+        return 0.2;
+    }
+
+}
